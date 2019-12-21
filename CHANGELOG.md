480b0bca7455d498b1663841e720f9c0a775d1ae
Hugo Parente Lima <hugo.pl@gmail.com>
2019-12-21 01:35:52 -0300

    Release v1.1.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [1.1.0] - 2019-12-21
+### Added
+- Try to use `git describe --tags` to generate better version strings for
+  development builds.
+
 ## [1.0.0] - 2019-11-18
 - Project name changed from the generic Version to VersionFromShard, to avoid
   name clashes with existing Version class/modules.
--- spec/version_from_shard_spec.cr
+++ spec/version_from_shard_spec.cr
@@ -9,6 +9,6 @@ describe VersionFromShard do
     # If we are not exactly in the tagged commit, the VERSION value are going
     # to be like `git describe --tags`. A complete test would call `crystal init`
     # somewhere, add some commits then test it...
-    Foo::VERSION.should start_with("1.0.0")
+    Foo::VERSION.should start_with("1.1.0")
   end
 end
